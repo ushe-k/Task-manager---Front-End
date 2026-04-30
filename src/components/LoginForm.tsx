c4e1d3c00e2e59131922568ad274d245f5c18720
Ushe Karengenu <ushek441@gmail.com>
2026-04-30 16:31:05 +0200

    Fix button className syntax in LoginForm

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -45,8 +45,8 @@ export default function LoginForm({  }) {
         {
           loading?
           (
-          <button class="btn btn-primary" type="button" disabled>
-            <span class="spinner-border spinner-border-sm" aria-hidden="true"></span>
+          <button className="btn btn-primary" type="button" disabled>
+            <span className="spinner-border spinner-border-sm" aria-hidden="true"></span>
             <span role="status">Loading...</span>
           </button>
           ):
